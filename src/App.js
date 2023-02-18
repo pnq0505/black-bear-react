786e3470384936bab6e1f2527b7b208380d44e05
Quoc Pham <quoc.pham@manifera.com>
2023-02-18 10:57:01 +0700

    Build a simple React app that allows viewing and interacting with a grid of curated photos from Unsplash

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import { Gallery } from "react-grid-gallery";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [index, setIndex] = useState(null);
+  const [slides, setSlides] = useState([]);
+
+  const handleClick = useCallback((index, item) => {
+    setIndex(index);
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.unsplash.com/photos/?client_id=v5M8YbalIAM6nS2IU07YMKKc1UGlaqkIVJOT69R-hnk&page=2"
+      )
+      .then((res) => {
+        const data = res.data;
+        const photoLinks = data.map((d) => {
+          return {
+            src: d.urls.raw,
+            width: d.width,
+            height: d.height,
+            alt: d.alt_description,
+          };
+        });
+        setImages(photoLinks);
+        const slides = photoLinks.map((photo) => {
+          return { ...photo, srcSet: photoLinks };
+        });
+        console.log("slides :>> ", slides);
+        setSlides(slides);
+      });
+  }, []);
+
+  const handleSelect = useCallback(
+    (index, item, event) => {
+      const nextImages = images.map((image, i) =>
+        i === index ? { ...image, isSelected: !image.isSelected } : image
+      );
+      setImages(nextImages);
+    },
+    [images]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Gallery images={images} onSelect={handleSelect} onClick={handleClick} />
+      {index && (
+        <Lightbox
+          open={index}
+          close={() => {
+            setIndex(null);
+          }}
+          slides={slides}
+          index={index}
+        />
+      )}
+    </>
   );
 }
 
